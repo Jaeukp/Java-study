1ca276df481dc64fe816c3d2fd467b14d50ec9f9
jaeuk Park <mertake7@naver.com>
2023-07-25 17:18:11 +0900

    update

---
+++ src/Solution.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class Solution {
+//        public int solution(int a, int b) {
+//            int ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+//            int ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+//
+//            if (ab >= ba) {
+//                return ab;
+//            } else {
+//                return ba;
+//            }
+//        }
+//    }
+//    public static String solution(String[] arr) {
+//        StringBuilder result = new StringBuilder();
+//
+//        for (String str : arr) {
+//            result.append(str);
+//        }
+//
+//        return result.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//        String[] arr = {"a", "b", "c", "d", "e", "f"};
+//        String result = solution(arr);
+//        System.out.println(result); // HelloWorld!WelcometoOpenAI
+//    }
+//}
+
+//    public static String solution(String str1, String str2) {
+//        StringBuilder result = new StringBuilder();
+//        int length = Math.min(str1.length(), str2.length());
+//
+//        for (int i = 0; i < length; i++) {
+//            result.append(str1.charAt(i)).append(str2.charAt(i));
+//        }
+//
+//        if (str1.length() > str2.length()) {
+//            result.append(str1.substring(length));
+//        } else if (str2.length() > str1.length()) {
+//            result.append(str2.substring(length));
+//        }
+//
+//        return result.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//        String str1 = "abc";
+//        String str2 = "xyz";
+//        String result = solution(str1, str2);
+//        System.out.println(result); // axbycz
+//    }
+//}
+
+//        str1 = "aaaaa";
+//        str2 = "bbbbb";
+//        String answer = "";
+//
+//        for (int i = 0; i < str1.length(); i++) {
+//            answer += str1.charAt(i);
+//            answer += str2.charAt(i);
+//        }
+//        System.out.println(answer);
+//        return answer;
+//    }
+
+
+
+
+//        System.out.println(answer);
+//        // abcabcabcabc 를 출력
+//        String str1 = "aaaaa";
+//        String str2 = "bbbbb";
+//        String str3 = "ccccc";
+//        String answer = "";
+//
+//        for (int i = 0; i < str1.length(); i++) {
+//            answer += str1.charAt(i);
+//            answer += str2.charAt(i);
+//            answer += str3.charAt(i);
+//        }
+//
+//        System.out.println(answer);
+    }
+
+
